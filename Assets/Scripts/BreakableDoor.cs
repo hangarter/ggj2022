62bc16b61d9a0d925a7aa25259bd4fc57ad9bc15
Hangarter <madison.paz@gmail.com>
2022-01-22 14:17:59 +0100

    Completed door logic

--- Assets/Scripts/BreakableDoor.cs
+++ Assets/Scripts/BreakableDoor.cs
@@ -6,16 +6,47 @@ using UnityEngine;
 public class BreakableDoor : MonoBehaviour
 {
     public List<GameObject> doorPrefabs;
+    public AudioClip crackingNoise;
+    public AudioClip breakingNoise;
 
+    private AudioSource audioSource;
     private int currentDoor;
 
     public void HitDoor()
     {
+        if (LastHit())
+        {
+            PlayBreakDoorNoise();
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            PlayCrackingNoise();
+        }
         ShowNextDoor();
+    }
+
+    private void PlayCrackingNoise()
+    {
+        if (currentDoor > 0)
+        {
+            audioSource.PlayOneShot(crackingNoise);
+        }
+    }
+
+    private void PlayBreakDoorNoise()
+    {
+        audioSource.PlayOneShot(breakingNoise);
+    }
+
+    private bool LastHit()
+    {
+        return currentDoor >= doorPrefabs.Count;
     }
 
     private void Start()
     {
+        audioSource = Camera.main.GetComponent<AudioSource>();
         currentDoor = 0;
         ShowNextDoor();
     }
